d0d452bc059efb94c8073edbda1e36040b07c699
loriqing <490855882@qq.com>
2018-01-05 12:01:33 +0800

    update
    GradeDao IGradeDao GradeService加注释

--- Daos/IGradeDao.cs
+++ Daos/IGradeDao.cs
@@ -5,21 +5,25 @@ using Xmu.Crms.Shared.Models;
 
 namespace Xmu.Crms.Services.ViceVersa.Daos
 {
+    /// <summary>
+    /// 成绩处理模块Dao层实现
+    /// @author Group ViceVersa
+    /// </summary>
     public interface IGradeDao
     {
         /// <summary>
         /// 按topicId删除学生打分表.
-        /// @author zhouzhongjun
+        /// @author Group ViceVersa
         /// </summary>
         /// <param name="topicId">话题Id</param>
+        /// <exception cref="T:Xmu.Crms.Shared.Exceptions.GroupNotFoundException">未找到小组</exception>
         void DeleteStudentScoreGroupByTopicId(long topicId);
 
         ///<summary>
         ///获取某学生一堂讨论课信息
-        ///@author qinlingyun
+        ///@author Group ViceVersa
         ///获取某学生一堂讨论课的详细信息（包括成绩）
         /// </summary>
-        /// <param name="userId">学生Id</param>
         /// <param name="seminarGroupId">讨论课小组Id</param>
         /// <returns>seminarGroup 讨论课小组信息（包括成绩）</returns>
         /// <exception cref="T:System.ArgumentException">id格式错误</exception>
@@ -27,64 +31,48 @@ namespace Xmu.Crms.Services.ViceVersa.Daos
         SeminarGroup GetSeminarGroupBySeminarGroupId(long seminarGroupId);
 
         /// <summary>
-        /// 按课程id获取学生该课程所有讨论课
-        /// @author qinlingyun
-        /// 通过课程id获取该课程下学生所有讨论课详细信息（包括成绩）
+        /// 不用写，调用其他Dao的方法
         /// </summary>
-        /// <param name="userId">学生id</param>
-        /// <param name="courseId">课程id</param>
-        /// <returns>list 该课程下所有讨论课列表</returns>
-        /// <exception cref="T:System.ArgumentException">id格式错误</exception>
-        /// <seealso cref="M:Xmu.Crms.Shared.Service.ISeminarService.ListSeminarByCourseId(System.Int64)"/>
-        /// <seealso cref="M:Xmu.Crms.Shared.Service.IGradeService.ListSeminarGradeByUserId(System.Int64)"/>
-        /// <seealso cref="M:Xmu.Crms.Shared.Service.ISeminarGroupService.ListSeminarGroupBySeminarId(System.Int64)"/>
         List<SeminarGroup> ListSeminarGradeByCourseId(long userId, long courseId);
 
         /// <summary>
         /// 提交对其他小组的打分.
-        /// @author Huhui
+        /// @author Group ViceVersa
         /// </summary>
-        /// <param name="userId">用户id</param>
-        /// <param name="topicId">话题Id</param>
-        /// <param name="seminarId">讨论课Id</param>
-        /// <param name="groupId">小组Id</param>
-        /// <param name="grade">分数</param>
+        /// <param name="seminarGroupTopic"></param>
+        /// <param name="userInfo"></param>
+        /// <param name="grade"></param>
         void InsertGroupGradeByUserId(SeminarGroupTopic seminarGroupTopic, UserInfo userInfo, int grade);
 
         /// <summary>
         /// 按ID设置小组报告分.
-        /// @author Huhui
+        /// @author Group ViceVersa
         /// </summary>
         /// <param name="seminarGroupId">讨论课组id</param>
         /// <param name="grade">分数</param>
+        /// <exception cref="T:System.GroupNotFoundException">该小组未找到</exception>
         void UpdateGroupByGroupId(long seminarGroupId, int grade);
 
         /// <summary>
-        /// 获取某学生的讨论课成绩列表.
-        /// @author qinlingyun
+        /// 不用写，在SeminarGroupService调用 IList<SeminarGroup> ListSeminarGroupIdByStudentId(long userId);
         /// </summary>
-        /// <param name="userId">用户id</param>
-        /// <seealso cref="M:Xmu.Crms.Shared.Service.ISeminarGroupService.ListSeminarGroupBySeminarId(System.Int64)"/>
         List<SeminarGroup> ListSeminarGradeByStudentId(long userId);
 
         /// <summary>
-        /// 定时器方法:讨论课结束后计算展示得分.
-        /// @author qinlingyun
-        /// 条件: 讨论课已结束  *GradeService
+        /// 根据讨论课Id和话题计算展示成绩，排序打分
         /// </summary>
-        /// <param name="seminarId">讨论课id</param>
-        /// <param name="seminarGroupId">讨论课组id</param>
-        /// <exception cref="T:System.ArgumentException">id格式错误</exception>
+        /// <param name="seminarId"></param>
+        /// <param name="topicList"></param>
         void CountPresentationGrade(long seminarId, IList<Topic> topicList);
 
         /// <summary>
         /// 定时器方法:讨论课结束后计算本次讨论课得分.
-        /// @author qinlingyun
+        /// @author Group ViceVersa
         /// 条件: 讨论课已结束，展示得分已算出  *GradeService
         /// </summary>
         /// <param name="seminarId">讨论课id</param>
-        /// <param name="seminarGroupList">讨论课小组列表</param>
-        /// <exception cref="T:System.ArgumentException">id格式错误</exception>
+        /// <param name="seminarGroupList">讨论课小组序列</param>
+        /// <exception cref="T:System.GroupNotFoundException">讨论课小组未找到错误</exception>
         void CountGroupGradeBySerminarId(long seminarId, IList<SeminarGroup> seminarGroupList);
     }
 }
